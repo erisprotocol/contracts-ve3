9f33f10bc4760303ae078cb3709b6da1554d0f15
Philipp <philipp@erisprotocol.com>
2024-10-10 14:04:56 +0200

    Update schema_pdt.rs

--- contracts/phoenix-treasury/examples/schema_pdt.rs
+++ contracts/phoenix-treasury/examples/schema_pdt.rs
@@ -1,10 +1,11 @@
 use cosmwasm_schema::write_api;
-use ve3_shared::msgs_phoenix_treasury::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use ve3_shared::msgs_phoenix_treasury::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 
 fn main() {
   write_api! {
       instantiate: InstantiateMsg,
       execute: ExecuteMsg,
       query: QueryMsg,
+      migrate: MigrateMsg
   }
 }
